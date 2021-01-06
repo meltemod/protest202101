5be4315601b3513c6400071d3deda89fec6bc1e0
modabas <modabas@iu.edu>
2021-01-05 20:11:50 -0500

    update

--- 00_repo_folder_structure.R
+++ 00_repo_folder_structure.R
@@ -4,6 +4,10 @@
 bucket <- '/N/project/c19/protest202101'
 if(dir.exists(bucket)==FALSE){dir.create(bucket)} #create bucket loc if does not exist
 #open data location
-loc_data = file.path(bucket,'data')
-if(dir.exists(loc_data)==FALSE){dir.create(loc_data)} #create data_loc if does not exist
+loc = file.path(bucket,'data')
+if(dir.exists(loc_data)==FALSE){dir.create(loc)} #create data_loc if does not exist
+save.image('00_repository_folder_structure.RData')
+#open workflow location
+loc_wf = file.path(bucket,'workflow')
+if(dir.exists(loc_wf)==FALSE){dir.create(loc_wf)} #create data_loc if does not exist
 save.image('00_repository_folder_structure.RData')
